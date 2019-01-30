0856b9a2632cc90a927bee72b2dcb6ce0ebe6c91
Borja S <Borja S@SrBorch>
2019-01-30 18:54:56 +0100

    Práctica iniciada

---
+++ src/p04/vetstucom/P04Vetstucom.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p04.vetstucom;
+
+/**
+ *
+ * @author Borja S
+ */
+public class P04Vetstucom {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
